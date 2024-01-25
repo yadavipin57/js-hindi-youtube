5146339cf131b320ac81dae569371980d1e89486
Vipin Yadav <63735356+yadavipin57@users.noreply.github.com>
2024-01-25 16:23:41 +0530

    Update iterations.js

--- iterations.js
+++ iterations.js
@@ -1,3 +1,4 @@
+// Iterations
 // for Loop
 
 for (let i = 0; i <= 10; i++) {
@@ -14,3 +15,26 @@ for(let i=1; i<=10; i++){
 }
 
 // There is a also a "break" and "continue" keyword
+
+// while and do while loop
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
