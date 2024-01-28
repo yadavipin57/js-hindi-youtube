0bf96675e554af3b0ed80b8cde31340f09d018cc
Vipin Yadav <63735356+yadavipin57@users.noreply.github.com>
2024-01-28 12:51:17 +0530

    DOM

---
+++ dom1.js
@@ -0,0 +1,14 @@
+// Document Object Model
+
+// console.log(window);
+// console.log(window.document);
+// console.log(document);
+// console.dir(document);
+
+// console.log(document.links)
+// console.log(document.links[i]) // --> i is an +ve integer
+
+// document.getElementById('firstHeading')
+// document.getElementById('firstHeading').innerHTML = "<h1>Chai aur code</h1>"
+
+
