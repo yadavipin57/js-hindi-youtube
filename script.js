f33bb9bd419a963e8bac6929b761e064ffb50df4
Vipin Yadav <63735356+yadavipin57@users.noreply.github.com>
2024-01-31 17:35:02 +0530

    Digital clock

--- script.js
+++ script.js
@@ -1,42 +1,11 @@
 // Logic
 
-// select button, input
+// select
 
-const form = document.querySelector('form')
-const clear = document.querySelector('.clear')
-const height = document.querySelector('#height')
-const weight = document.querySelector('#weight')
-const range = document.querySelector('.range');
-// Events on Forms
+const time = document.querySelector('.time');
 
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const valOfHeight = parseInt(document.querySelector('#height').value)
-    const valOfWeight = parseInt(document.querySelector('#weight').value)
-    const results = document.querySelector('.showBMI')
-
-    if(valOfHeight === '' || valOfHeight <= 0){
-        results.innerHTML = `Please enter a valid height in cm`
-    }else if(valOfWeight === '' || valOfWeight <= 0){
-        results.innerHTML = `Please enter a valid weight in kg`
-    }else{
-        const bmi = (valOfWeight / ((valOfHeight * valOfHeight)/10000)).toFixed(2);
-        results.innerHTML = `<span>${bmi}</span>`;
-        if(bmi<18.5){
-            range.innerHTML = 'Under Weight'
-        }else if(bmi>=18.5 && bmi<25){
-            range.innerHTML = 'Normal Weight'
-        }else if(bmi>=25 && bmi<30){
-            range.innerHTML = 'Over Weight'
-        }else if(bmi>=30){
-            range.innerHTML = 'Obesity'
-        }
-    }
-})
-
-// Event on clear button
-clear.addEventListener('click', ()=>{
-    range.innerHTML = ''
-    height.value = '';
-    weight.value = '';
-})
+// Show time
+setInterval(()=>{
+    const date = new Date();
+    time.innerHTML = date.toLocaleTimeString();
+}, 1000)
