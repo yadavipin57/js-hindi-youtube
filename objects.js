512b23552b5c0ba29fde2fb649bc56d0f81d050a
Vipin Yadav <63735356+yadavipin57@users.noreply.github.com>
2024-02-09 14:10:57 +0530

    Objects

--- objects.js
+++ objects.js
@@ -1,39 +1,50 @@
-// singleton
-// Object.create
+// Javascript has a default behaviour of Prototypal behaviour. Which means if it has to find any thing it will go to parents then its parents an so on until it gets it or it gets the null value
 
-// object literals
-// below is the example of object, it takes values in key: value pairs
+// Arrow functions doest have axis of "this" because of this prototypal behaviour
 
-    // declaring Symbol datatype
-const mySym = Symbol("key1")
-const jsUser = {
-    name: "Vipin", // in key: value pairs, key is always stored as a string
-    "full name": "Vipin Yadav", // here "full name" is declared as string as well, which can only be accesses using the second method given below
-    age: 22,
-    [mySym]: "myKey1",
-    location: "Mumbai",
-    email: "yadavipin57@gmail.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Mon", "Wed"]
+// End of the day everything in JS is an object. And any property which is available to Object is also available to other things like Array, String etc because of inheritance. Whether those proprties are usable or not its a different thing
+
+// Function is a function but it is also a object. Which means function can also be used or acccessed as object
+
+function multipleBy5(num){
+    return num*5
 }
 
-// To access the object elements there are two ways, given below
-console.log(jsUser.email) // method 1
-console.log(jsUser["email"]) // method 2
-console.log(jsUser["full name"]) // method 2
-console.log(jsUser[mySym]) // this is how symbol is accessed from the object
-console.log(typeof jsUser[mySym])
+// multipleBy5(5)
+multipleBy5.power = 2
 
-jsUser.email = "yadavipin57@yahoo.com" // to change the value of email key
-jsUser.greetings = function(){ // to add new key and value, here be are adding function as a value
-    console.log(`Hello JS User, ${this.name}`)
+console.log(multipleBy5(5));
+console.log(multipleBy5.power);
+console.log(multipleBy5.prototype); // {}
+
+function createUser(username, score){
+    this.username = username
+    this,score = score
 }
-console.log(jsUser.greetings);
-console.log(jsUser.greetings());
 
-console.log(jsUser.email);
-Object.freeze(jsUser) // to freeze the values of object, and it cant be changed letter
-jsUser.email = "yadavipin57@microsoft.com" // attempting to change the email value, but it wont be implemented
-console.log(jsUser.email);
-console.log(jsUser);
+createUser.prototype.increment = function(){ // creating are own prototype
+    this.score++ // Jisne bhi bola hai uska score badha do
+}
+createUser.prototype.printMe = function(){
+    console.log(`score is ${this.score}`); // Jisne bhi bola hai uska score dikha do
+}
 
+const user1 = new createUser('user1', 25) // Without 'new' there is an error
+const user2 = createUser('user2', 250)
+
+user1.printMe() // instead of writting "user1.prototype.printMe()" we only wrote "user1.printMe()", because JS gives us this syntactical sugar
+
+// This file mainly explains about the "new" keyword
+/*
+
+Here's what happens behind the scenes when the 'new' keyword is used:
+
+A new object is created: The new keyword initiates the creation of a new JavaScript object.
+
+A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. This means that it has access to properties and methods defined on the constructor's prototype.
+
+The constructor is called: The constructor function is called with the specified arguments and this is bound to the newly created object. If no explicit return value is specified from the constructor, JavaScript assumes this, the newly created object, to be the intended return value.
+
+The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
+
+*/
